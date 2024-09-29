46e6c2c4b391b094774233616b158f511e067022
Nguyễn Lộc Tỷ <uguest17@gmail.com>
2024-09-30 02:33:00 +0700

    chore: code formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,26 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
 
 const chicagoFLF = localFont({
-  src: "../assets/fonts/ChicagoFLF.ttf",
-  variable: "--font-chicago",
-  weight: "100 900",
-});
+    src: '../assets/fonts/ChicagoFLF.ttf',
+    variable: '--font-chicago',
+    weight: '100 900',
+})
 
 export const metadata: Metadata = {
-  title: "QuanTrieuPCYT",
-  description: "17-year old Vietnamese who likes tinkering with tech things.",
-};
+    title: 'QuanTrieuPCYT',
+    description: '17-year old Vietnamese who likes tinkering with tech things.',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${chicagoFLF.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={`${chicagoFLF.variable} antialiased`}>{children}</body>
+        </html>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import MenuBar from "@/components/MenuBar";
-import Window from "@/components/Window";
-import Image from "next/image";
+import MenuBar from '@/components/MenuBar'
+import Window from '@/components/Window'
 
 export default function Home() {
-  return (
-    <div className="fixed w-screen h-screen bg-zinc-900 p-4">
-      <div className="relative w-full h-full pb-16 rounded-3xl">
-        <MenuBar />
-        <div id="content" className="relative w-full h-full rounded-b-3xl background-wrap">
-          <Window />
+    return (
+        <div className='fixed w-screen h-screen bg-zinc-900 p-4'>
+            <div className='relative w-full h-full pb-16 rounded-3xl'>
+                <MenuBar />
+                <div id='content' className='relative w-full h-full rounded-b-3xl background-wrap'>
+                    <Window />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
